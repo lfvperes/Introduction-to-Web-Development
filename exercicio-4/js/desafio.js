feabc59fa5e092273e698314e461d4a5ea587b1b
UniqueLoverJellyfish <luisfvperes@gmail.com>
2020-04-19 07:11:54 -0300

    Calculadora funcionando, falta corrigir bugs

--- exercicio-4/js/desafio.js
+++ exercicio-4/js/desafio.js
@@ -5,13 +5,72 @@ NroUSP: 10310641
 */
 
 var botoes = window.document.querySelectorAll("button");
-var entrada;
-for (botao of botoes){
-    entrada = botao;
-    entrada.addEventListener("click", display);
+var tela = window.document.querySelector("#conteudo");
+var conteudo = "0";
+var anterior = "";
+
+for (let botao of botoes){
+    botao.addEventListener("click", computar);
+}
+
+function computar(){
+    let botao = event.target;
+    let entrada = botao.innerText;
+    
+    switch(botao.className){
+        case "igual":
+            // ao apertar = após um operador, ele será ignorado
+            // ex: "3+4-" vira "3+4"
+            if(anterior.className == "operator"){
+                conteudo = conteudo.slice(0, conteudo.length-1);
+            }
+            // o botão é "x" por estética, mas a operação precisa ser "*"
+            conteudo = conteudo.replace(/x/g, "*");
+            // realizar o cáclulo
+            conteudo = String(eval(conteudo));
+            break;
+        case "operator":
+            // se um operador for digitado após outro, ele o substitui
+            // do contrário apenas é concatenado
+            // ex: "3+-" vira "3-"
+            if(anterior.className == "operator"){
+                conteudo = conteudo.slice(0, conteudo.length-1).concat(entrada);
+            }else{conteudo = conteudo.concat(entrada);}
+            break;
+        case "dot":
+            // evitar que o resultado de uma operação seja concatenado com um ponto
+            if(anterior.innerText == "="){
+                conteudo = "0";
+            }
+            // evitar que o número tenha mais de um ponto
+            if(!conteudo.includes(".")){
+                conteudo = conteudo.concat(entrada);
+            }
+            break;
+        case "C":
+            conteudo = "0";
+            break;
+        case "CE":
+            if(conteudo != "0" && anterior.innerText != "="){
+                conteudo = conteudo.slice(0, conteudo.length-1);
+            }
+            if(conteudo == ""){
+                conteudo = "0";
+            }
+            break;
+        default:
+            if(conteudo == "0" && entrada != "."||anterior.innerText == "="){
+                conteudo = "";
+            }
+            conteudo = conteudo.concat(entrada);
+            break;
+    }
+    display();
+    if(botao.className != "CE"){
+        anterior = botao;
+    }
 }
 
 function display(){
-    var tela = window.document.querySelector("#display");
-    tela.innerText = event.target.innerText;
+    tela.innerText = conteudo;
 }
