42fa0e9e39f8d65dfdf8c0a824b899b1e3caa8fa
UniqueLoverJellyfish <luisfvperes@gmail.com>
2020-04-19 00:48:13 -0300

    Na parte 4, adicionado fetch da segunda página + algumas otimizações

--- exercicio-4/js/parte4.js
+++ exercicio-4/js/parte4.js
@@ -1,23 +1,33 @@
+/*
+SCC0219 - Exercise 4, part 4
+Nome: LUIS FILIPE VASCONCELOS PERES
+NroUSP: 10310641
+*/
 var caixa = window.document.querySelector(".caixa");
 var botao = window.document.querySelector("#change");
 var hex = window.document.querySelector("#hex");
 var nome = window.document.querySelector("#nome");
 var cores;
-var corAtual;
 
 // listar as cores uma só vez
-fetch("https://reqres.in/api/unknown")
+fetch("https://reqres.in/api/unknown")  // item a)
 .then(response => response.json())
-.then(success => {
-    cores = success.data;
-})
+.then(success => cores = success.data)
+.then(fetch("https://reqres.in/api/unknown?page=2")
+.then(response => response.json())
+.then(success => cores = cores.concat(success.data)))
 .then(() => {botao.addEventListener("click", mudarCor)})
 
-
+/*
+    mudar para uma cor aleatória dentre as listadas,
+    escrever o nome,
+    escrever o código hexadecimal, pintar o fundo
+*/
 function mudarCor(){
-    corAtual = Math.floor(Math.random() * cores.length);
-    console.log(cores[corAtual].name, cores[corAtual].color);
-    nome.innerText = cores[corAtual].name;
-    hex.innerText = cores[corAtual].color;
-    caixa.style.backgroundColor = cores[corAtual].color;
+    let indice, corAtual;
+    indice = Math.floor(Math.random() * cores.length);
+    corAtual = cores[indice];
+    console.log(corAtual.id, corAtual.name, corAtual.color);
+    nome.innerText = corAtual.name;
+    caixa.style.backgroundColor = hex.innerText = corAtual.color;
 }
