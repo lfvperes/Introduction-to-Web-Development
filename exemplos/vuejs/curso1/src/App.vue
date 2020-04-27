a1a4549cfedbf8524d8986d9cfafd7cb4ff6c96d
UniqueLoverJellyfish <luisfvperes@gmail.com>
2020-04-27 14:59:05 -0300

    Aprendendo VUE js

---
+++ exemplos/vuejs/curso1/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Items v-bind:items="items"/>
+  </div>
+</template>
+
+<script>
+import Items from './components/Items';
+
+export default {
+  name: 'App',
+  components: {
+    Items
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: 'first item',
+          odd: true
+        },
+        {
+          id: 2,
+          title: 'second item',
+          odd: false
+        },
+        {
+          id: 3,
+          title: 'third item',
+          odd: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ exemplos/vuejs/curso1/src/components/Items.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="item in items">
+            <h3>{{item.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Items',
+    props: ['items']
+}
+</script>
+
+<style scoped>
+    
+</style>
